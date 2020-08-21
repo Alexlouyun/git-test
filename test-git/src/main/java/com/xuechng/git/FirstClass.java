83fbafd18bd11146f0e821250beceb1954c7ffe4
Alex <330205507@qq.com>
2020-08-21 17:48:19 +0800

    把文件提交到本地仓库

---
+++ test-git/src/main/java/com/xuechng/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.xuechng.git;
+
+/**
+ * Created by Alexlou on 2020/8/21.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
